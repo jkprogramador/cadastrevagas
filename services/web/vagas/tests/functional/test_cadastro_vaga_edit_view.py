344f4961700da112fd8ada57fcde673d39bcdc4e
Jack Lee <br.sp.jkemail@gmail.com>
2022-02-22 09:32:24 -0300

    Rewrite some tests for job opportunity edit view.

--- services/web/vagas/tests/functional/test_cadastro_vaga_edit_view.py
+++ services/web/vagas/tests/functional/test_cadastro_vaga_edit_view.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.forms import CharField, EmailField, URLField, Textarea
 from vagas.models import Vaga
 
@@ -11,16 +11,15 @@ class CadastroVagaEditViewTest(TestCase):
     So that I can edit details of a job opportunity I've registered
     """
 
-    @classmethod
-    def setUpClass(cls) -> None:
+    def setUp(self) -> None:
         """
-        GIVEN a job opportunity I've registered
+        GIVEN a previously registered job opportunity
 
         WHEN I go to /oportunidades/<ID of job opportunity>/edit
 
-        :return: None
+        :rtype: None
         """
-        cls.vaga = Vaga.objects.create(
+        self.vaga = Vaga.objects.create(
             empresa_nome='Minha empresa',
             empresa_endereco='Meu endereço',
             empresa_email='meuemail@email.com',
@@ -33,20 +32,14 @@ class CadastroVagaEditViewTest(TestCase):
             data_hora_entrevista='06/04/2022 09:35',
         )
 
-        cls.response = Client().get(f'/oportunidades/{cls.vaga.pk}/edit')
-        cls.form = cls.response.context['form']
-    
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.vaga.delete()
-        cls.response = None
-        cls.form = None
+        self.response = self.client.get(f'/oportunidades/{str(self.vaga.pk)}/edit')
+        self.form = self.response.context['form']
     
     def test_should_have_text_field_filled_with_nome_da_empresa(self) -> None:
         """
         THEN it should have a text field filled with the corresponding company's name
 
-        :return: None
+        :rtype: None
         """
         self.assertIsInstance(self.form.fields['empresa_nome'], CharField)
         self.assertEqual(self.vaga.empresa_nome, self.form.cleaned_data['empresa_nome'])
@@ -55,7 +48,7 @@ class CadastroVagaEditViewTest(TestCase):
         """
         THEN it should have a text field filled with the corresponding company's address
 
-        :return: None
+        :rtype: None
         """
         self.assertIsInstance(self.form.fields['empresa_endereco'], CharField)
         self.assertEqual(self.vaga.empresa_endereco, self.form.cleaned_data['empresa_endereco'])
@@ -64,7 +57,7 @@ class CadastroVagaEditViewTest(TestCase):
         """
         THEN it should have an email field filled with the corresponding company's email
 
-        :return: None
+        :rtype: None
         """
         self.assertIsInstance(self.form.fields['empresa_email'], EmailField)
         self.assertEqual(self.vaga.empresa_email, self.form.cleaned_data['empresa_email'])
@@ -73,35 +66,36 @@ class CadastroVagaEditViewTest(TestCase):
         """
         THEN it should have a URL field filled with the corresponding company's website URL
 
-        :return: None
+        :rtype: None
         """
         self.assertIsInstance(self.form.fields['empresa_site'], URLField)
         self.assertEqual(self.vaga.empresa_site, self.form.cleaned_data['empresa_site'])
     
     def test_should_have_phone_field_filled_with_telefone_celular_da_empresa(self) -> None:
         """
-        THEN it should have a phone field filled with the corresponding company's cellphone in the correct format
+        THEN it should have a phone field filled with the corresponding company's
+        cellphone in the correct format
 
-        :return: None
+        :rtype: None
         """
         self.assertEqual('tel', self.form.fields['empresa_telefone_celular'].widget.input_type)
-        self.assertEqual(self.vaga.empresa_telefone_celular, self.form.data['empresa_telefone_celular'])
+        self.assertEqual(self.vaga.empresa_telefone_celular, self.form.cleaned_data['empresa_telefone_celular'])
     
     def test_should_have_phone_field_filled_with_telefone_comercial_da_empresa(self) -> None:
         """
-        THEN it should have a phone field filled with the corresponding company's landline
+        THEN it should have a phone field filled with the corresponding company's
+        landline in the correct format
 
-        :return: None
+        :rtype: None
         """
-        pass
         self.assertEqual('tel', self.form.fields['empresa_telefone_comercial'].widget.input_type)
-        self.assertEqual(self.vaga.empresa_telefone_comercial, self.form.data['empresa_telefone_comercial'])
+        self.assertEqual(self.vaga.empresa_telefone_comercial, self.form.cleaned_data['empresa_telefone_comercial'])
     
     def test_should_have_text_field_filled_with_titulo_do_cargo(self) -> None:
         """
         THEN it should have a text field filled with the corresponding job title
 
-        :return: None
+        :rtype: None
         """
         self.assertIsInstance(self.form.fields['cargo_titulo'], CharField)
         self.assertEqual(self.vaga.cargo_titulo, self.form.cleaned_data['cargo_titulo'])
@@ -110,33 +104,35 @@ class CadastroVagaEditViewTest(TestCase):
         """
         THEN it should have a text area filled with the corresponding job description
 
-        :return: None
+        :rtype: None
         """
         self.assertIsInstance(self.form.fields['cargo_descricao'].widget, Textarea)
         self.assertEqual(self.vaga.cargo_descricao, self.form.cleaned_data['cargo_descricao'])
     
     def test_should_have_url_field_filled_with_site_referencia(self) -> None:
         """
-        THEN it should have a URL field filled with the corresponding reference site
+        THEN it should have a URL field filled with the corresponding website where
+        the opportunity was found
 
-        :return: None
+        :rtype: None
         """
         self.assertIsInstance(self.form.fields['site_referencia'], URLField)
         self.assertEqual(self.vaga.site_referencia, self.form.cleaned_data['site_referencia'])
     
     def test_should_have_datetime_field_filled_with_data_e_hora_da_entrevista(self) -> None:
         """
-        THEN it should have a datetime field filled with the corresponding date and time of the interview
+        THEN it should have a datetime field filled with the corresponding date and time
+        of the job interview
 
-        :return: None
+        :rtype: None
         """
         self.assertEqual('datetime', self.form.fields['data_hora_entrevista'].widget.input_type)
         self.assertEqual(self.vaga.data_hora_entrevista, self.form.cleaned_data['data_hora_entrevista'])
     
-    def test_should_have_a_way_to_submit_form(self) -> None:
+    def test_should_have_a_button_to_submit_the_form(self) -> None:
         """
-        THEN it should have a way to submit the form
+        THEN it should have a button to submit the form
 
-        :return: None
+        :rtype: None
         """
         self.assertContains(self.response, 'type=submit')
--- services/web/vagas/tests/unit/test_oportunidades_edit_view.py
+++ services/web/vagas/tests/unit/test_oportunidades_edit_view.py
@@ -1,14 +1,14 @@
-from django.test import SimpleTestCase
-from django.urls import reverse
+from django.test import TestCase
+from django.urls import reverse, resolve
 from django.forms import Form
 from vagas.models import Vaga
+from vagas.views import edit_view
 
-class OportunidadesEditViewTest(SimpleTestCase):
+class OportunidadesEditViewTest(TestCase):
     """Test to ensure that edit view for a job opportunity works."""
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.vaga = Vaga.objects.create(
+    def setUp(self) -> None:
+        self.vaga = Vaga.objects.create(
             empresa_nome='Minha empresa',
             empresa_endereco='Meu endereço',
             empresa_email='meuemail@email.com',
@@ -20,43 +20,38 @@ class OportunidadesEditViewTest(SimpleTestCase):
             site_referencia='https://sitereferencia.com.br',
             data_hora_entrevista='04/05/2022 9:52',
         )
+        url = reverse('oportunidades_edit', args=[str(self.vaga.pk)])
+        self.response = self.client.get(url)
     
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.vaga.delete()
-    
-    def test_url_exists_at_proper_location(self) -> None:
+    def test_status_code(self) -> None:
         """
-        Ensure that the URL for the view exists.
+        Ensure that the view returns the correct status code.
 
-        :return: None
+        :rtype: None
         """
-        response = self.client.get(f'/oportunidades/{self.vaga.pk}/edit')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.response.status_code)
     
-    def test_view_url_accessible_by_name(self) -> None:
-        """
-        Ensure that the URL is accessible by name.
-
-        :return: None
-        """
-        response = self.client.get(reverse('oportunidades_edit', args=[str(self.vaga.pk)]))
-        self.assertEqual(200, response.status_code)
-    
-    def test_view_uses_correct_template(self) -> None:
+    def test_template(self) -> None:
         """
         Ensure that the view uses the correct template.
 
-        :return: None
+        :rtype: None
         """
-        response = self.client.get(reverse('oportunidades_edit', args=[str(self.vaga.pk)]))
-        self.assertTemplateUsed(response, 'oportunidades_edit.html')
+        self.assertTemplateUsed(self.response, 'oportunidades_edit.html')
     
-    def test_view_provides_form_object(self) -> None:
+    def test_provides_form_object(self) -> None:
         """
         Ensure that the view provides a form object to the template.
 
-        :return: None
+        :rtype: None
         """
-        response = self.client.get(reverse('oportunidades_edit', args=[str(self.vaga.pk)]))
-        self.assertIsInstance(response.context['form'], Form)
+        self.assertIsInstance(self.response.context['form'], Form)
+    
+    def test_url_resolves_to_correct_view(self) -> None:
+        """
+        Ensure that the given URL path resolves to the correct view.
+
+        :rtype: None
+        """
+        view = resolve(f'/oportunidades/{str(self.vaga.pk)}/edit')
+        self.assertEqual(edit_view.__name__, view.func.__name__)
