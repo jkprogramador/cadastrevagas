d608058eab9b0c15c82b7b2e207cc64c4acf4f7a
Jack Lee <br.sp.jkemail@gmail.com>
2022-02-22 09:57:51 -0300

    Rewrite tests for phone_formatter filter.

--- services/web/vagas/tests/unit/test_phone_formatter_filter.py
+++ services/web/vagas/tests/unit/test_phone_formatter_filter.py
@@ -8,18 +8,18 @@ class PhoneFormatterFilterTest(unittest.TestCase):
         """
         Ensure that phone formatter filter returns correct format for a cellphone.
 
-        :return: None
+        :rtype: None
         """
-        cellphone = '11987653201'
-        formatted_cellphone = phone_formatter(cellphone)
-        self.assertEqual('(11) 98765-3201', formatted_cellphone)
+        expected = '(11) 98765-3201'
+        actual = phone_formatter('11987653201')
+        self.assertEqual(expected, actual)
     
     def test_phone_formatter_returns_correct_landline_format(self) -> None:
         """
         Ensure that phone formatter filter returns correct format for a landline.
 
-        :return: None
+        :rtype: None
         """
-        landline = '1187653201'
-        formatted_landline = phone_formatter(landline)
-        self.assertEqual('(11) 8765-3201', formatted_landline)
+        expected = '(11) 8765-3201'
+        actual = phone_formatter('1187653201')
+        self.assertEqual(expected, actual)
